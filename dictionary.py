aaf11b8ec1a1f4c16b0b2308a2c246cb4dfce797
Sid <siddharth_shah@live.com>
2019-02-08 10:20:01 +0800

    Changed output from table schem to pandas dataframe so that it can be easily modified later on as features grow and converted to different forms

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,25 @@
+import json
+import numpy as np
+import pandas as pd
 from tableschema import Table
+from dateutil.parser import parse
 
 def infer(path, limit = 2000):
     table = Table(path)
     table.infer(limit=limit, confidence=0.75)
-    return table.schema
+    return convert_to_df(table.schema)
+
+def convert_to_df(schema):
+    names = ["name"]
+    types = ["type"]
+    formats = ["format"]
+
+    for field in schema.fields: 
+        names.append(field.name)
+        types.append(field.type)
+        formats.append(field.format)
+
+    data = np.array([names, types, formats])
+    df = pd.DataFrame(data=data[1:,1:], index=data[1:,0], columns=data[0,1:])
+    df.index.name = "attribute"
+    return df
--- test_dictionary.py
+++ test_dictionary.py
@@ -1,21 +1,15 @@
 import os
 import unittest
 from dictionary import infer
+import pandas as pd
 
 class TestDictionary(unittest.TestCase):
 
     def test_infer(self):
-        schema = infer(os.getcwd() + "/data/sample.csv")
-        
-        self.assertEqual(schema.fields[0].name, "int" )
-        self.assertEqual(schema.fields[1].name, "float" )
-        self.assertEqual(schema.fields[2].name, "text" )
-        self.assertEqual(schema.fields[3].name, "date_time" )
-
-        self.assertEqual(schema.fields[0].type, "integer" )
-        self.assertEqual(schema.fields[1].type, "number" )
-        self.assertEqual(schema.fields[2].type, "string" )
-        self.assertEqual(schema.fields[3].type, "string" )
+        actual = infer(os.getcwd() + "/data/sample.csv")
+        expected = pd.read_csv(os.getcwd() + "/data/schema.csv")
+        expected.set_index("attribute", inplace=True)
+        self.assertTrue(expected.equals(actual))
 
 if __name__ == '__main__':
     unittest.main()
